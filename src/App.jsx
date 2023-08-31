f15664216dd6989c7a40016ca3fd6ac9dbc4100b
Adonu, Anyewe Ernest <principlesbrothers@yahoo.com>
2023-08-31 22:03:41 +0000

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/header/Header';
+import SubHeader from './components/subheader/SubHeader';
+import HospitalDetails from './components/hospitals/HospitalDetails';
 
-import './App.css'
+import './App.css';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <>
-      <h1>Top Hospital</h1>
+      <Header />
+      <SubHeader />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/hospital/:id' element={<HospitalDetails />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
